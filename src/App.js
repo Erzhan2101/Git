86e58ce80537c89e3ba6cffbceaccd7a7ee57839
Erzhan2101 <erzhan210198@gmail.com>
2021-09-23 21:05:00 +0600

    projeckt start

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
+import {BrowserRouter as Router, Route} from "react-router-dom";
+import Home from "./views/Home";
+import Readme from "./views/Readme";
+import Repositories from "./views/Repositories";
 
 function App() {
-  return (
-    <div >
-     app
-    </div>
-  );
+    return (
+        <Router>
+            <Route exact path="/"><Home /></Route>
+            <Route exact path="/:username"><Readme /></Route>
+            <Route path="/:username/:project"><Repositories /></Route>
+        </Router>
+    );
 }
 
 export default App;
---
+++ src/views/Home/index.js
@@ -0,0 +1,14 @@
+import React, {useState} from 'react';
+
+const Home = () => {
+    const [username, setUsername] = useState("")
+
+
+    return (
+        <>
+
+        </>
+    );
+};
+
+export default Home;
